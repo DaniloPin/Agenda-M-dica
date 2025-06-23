f4cfb2fe0412e282996101a9b0afe6cb20a7fb37
Danilo Gonçalves Pinheiro <124277720+DaniloPin@users.noreply.github.com>
2025-06-23 19:36:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Consulta consulta = new Consulta("Danilo Pinheiro", "Débora", new DateTime(2025, 5, 20));
+
+consulta.ExibirResumo();
+
+consulta.Reagendar(new DateTime(2025, 5, 25));
+
+consulta.ExibirResumo();
